569658faa77bef24215b610876182f4324c065ad
JJ Merelo <jjmerelo@gmail.com>
2018-06-30 19:43:41 +0200

    Adds perl6 use vs issues

--- script/sankey-filtered.R
+++ script/sankey-filtered.R
@@ -1,11 +1,11 @@
 library("networkD3")
 library("igraph")
 
-data <- read_graph("../processed-data/agegroup-vs-perluse.net",format="pajek")
+data <- read_graph("../processed-data/perl6use-vs-issues.net",format="pajek")
 filtered <- delete.edges(data, which(E(data)$weight <=1)) 
 smaller <- delete.vertices(filtered,which(degree(filtered)<1))
 survey <-  igraph_to_networkD3(smaller)
 sn <- sankeyNetwork(Links = survey$links, Nodes = survey$nodes, Source = "source",
                     Target = "target", Value = "value", NodeID = "name",
                     units = "answers", fontSize = 20, nodeWidth=30)
-sn %>% saveNetwork( file= '../html/sankey-agegroup-vs-perluse.html' )
+sn %>% saveNetwork( file= '../html/sankey-perl6use-vs-issues.html' )
