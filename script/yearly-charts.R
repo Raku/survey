154a6a2f4e8003d984e1777f269d8c2323f2e2f9
JJ Merelo <jjmerelo@gmail.com>
2020-09-20 10:40:02 +0200

    Adds doc score

--- script/yearly-charts.R
+++ script/yearly-charts.R
@@ -13,3 +13,7 @@ use.perl <- data %>%  count( Have.you.used.Perl.extensively.before.finding.out.a
 
 ggplot(use.perl, aes(year,n,fill=Have.you.used.Perl.extensively.before.finding.out.about.Raku.))+ geom_bar( stat='identity',position='stack')+theme_tufte()
 ggsave("../illos/use-perl-years.png",width=8,height=6)
+
+data$year <- as.factor(data$year)
+ggplot(data, aes(x=year,y=How.would.you.rate.the.Raku.documentation.,group=year))+ geom_boxplot()+stat_summary(fun=mean, geom="point", shape=20, size=12, color="red", fill="red")+theme_tufte()
+ggsave("../illos/raku-doc-score-years.png",width=8,height=6)
