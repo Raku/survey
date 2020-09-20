037cba6128753530187620708890177ca2ed9919
JJ Merelo <jjmerelo@gmail.com>
2020-09-20 10:18:31 +0200

    Add use prior use of Perl

--- script/yearly-charts.R
+++ script/yearly-charts.R
@@ -8,3 +8,8 @@ age.year <- data %>%  count( What.age.group.do.you.fall.into., year )
 
 ggplot(age.year, aes(year,n,fill=What.age.group.do.you.fall.into.))+ geom_bar( stat='identity',position='stack')
 ggsave("../illos/age-groups-years.png",width=8,height=6)
+
+use.perl <- data %>%  count( Have.you.used.Perl.extensively.before.finding.out.about.Raku., year )
+
+ggplot(use.perl, aes(year,n,fill=Have.you.used.Perl.extensively.before.finding.out.about.Raku.))+ geom_bar( stat='identity',position='stack')+theme_tufte()
+ggsave("../illos/use-perl-years.png",width=8,height=6)
