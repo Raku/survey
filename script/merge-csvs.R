5389b24cf6f3d57db4e9e8f4d50c6662ade13d09
JJ Merelo <jjmerelo@gmail.com>
2020-09-20 09:36:13 +0200

    Merges common columns (beta)

--- script/merge-csvs.R
+++ script/merge-csvs.R
@@ -2,13 +2,17 @@ library( dplyr )
 
 data.1 <- read.csv("../June2018/p6survey.csv")
 data.2 <- read.csv("../June2019/p6survey.csv")
+data.3 <- read.csv("../August2020/raku-survey.csv")
+
 data.1$year <- "2018"
 data.2$year <- "2019"
+data.3$year <- "2020"
 
-common <- intersect(colnames(data.1),colnames(data.2))
+common <- intersect( intersect(colnames(data.1),colnames(data.2)),colnames(data.3) )
 
 data.1.common <- data.1 %>% select( common )
 data.2.common <- data.2 %>% select( common )
+data.3.common <- data.3 %>% select( common )
 
-write.csv( bind_rows ( data.1.common, data.2.common ), "data-2018-2018.csv" )
+write.csv( bind_rows ( data.1.common, data.2.common, data.3.common ), "data-2018-2020.csv" )
 
